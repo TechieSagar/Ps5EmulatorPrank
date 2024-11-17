8e22014f13c00359cc05210d6df8b495635ca45a
TechieSagar <sk19966313@gmail.com>
2024-11-17 15:23:15 +0530

    upload ui added

---
+++ app/src/main/java/com/techietech/ps5emulatorprank/GameDataClass.java
@@ -0,0 +1,33 @@
+package com.techietech.ps5emulatorprank;
+
+public class GameDataClass {
+    private String gameName;
+    private String gameUrl;
+    private String gameImage;
+    private String gamePlatform;
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public String getGameUrl() {
+        return gameUrl;
+    }
+
+    public String getGameImage() {
+        return gameImage;
+    }
+
+    public String getGamePlatform() {
+        return gamePlatform;
+    }
+
+    public GameDataClass(String gameName, String gameUrl, String gameImage, String gamePlatform) {
+        this.gameName = gameName;
+        this.gameUrl = gameUrl;
+        this.gameImage = gameImage;
+        this.gamePlatform = gamePlatform;
+    }
+
+
+}
--- app/src/main/java/com/techietech/ps5emulatorprank/MainActivity.java
+++ app/src/main/java/com/techietech/ps5emulatorprank/MainActivity.java
@@ -4,47 +4,37 @@ import static com.denzcoskun.imageslider.constants.ScaleTypes.FIT;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
-import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
-import com.google.android.gms.ads.AdError;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.FullScreenContentCallback;
-import com.google.android.gms.ads.LoadAdError;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.annotations.concurrent.Background;
 import com.google.firebase.appcheck.FirebaseAppCheck;
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.techietech.ps5emulatorprank.Adapters.OnBoardingAdapter;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button nextBtn,nextScreen;
+    Button nextBtn,nextScreen, play_button, upload_button;
+    FloatingActionButton fab;
     LinearLayout dotsLayout;
     ViewPager viewPager;
     private InterstitialAd mInterstitialAd;
@@ -65,15 +55,24 @@ public class MainActivity extends AppCompatActivity {
         //dotsLayout = findViewById(R.id.dotsLayout);
         //viewPager = findViewById(R.id.slider);
 
-        //loadInterstitialAd();
+        play_button = findViewById(R.id.play_button);
+        upload_button = findViewById(R.id.upload_button);
+        fab = findViewById(R.id.fab);
 
-//        nextScreen.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //openActivity2();
-//                //showInterstitialAd();
-//            }
-//        });
+        play_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UploadActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //ScreenView = findViewById(R.id.relativeLayout);
 
